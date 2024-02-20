204bb401fdd6a415ad1e416e4491b3d0bd9310cb
Alejandro150900 <luisaa00-@hotmail.com>
2024-02-19 20:54:37 -0600

    Semana 5 proyecto tienda

---
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tienda.service.impl;
+ 
+import com.tienda.dao.CategoriaDao;
+import com.tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.tienda.service.CategoriaService;
+ 
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+    
+    //La anotación autowired crea un único objeto mientras se ejecuta la app
+    @Autowired
+    private CategoriaDao categoriaDao;
+    
+    public List<Categoria> getCategorias(boolean activos){
+        var lista=categoriaDao.findAll();
+        if(activos){ //Se deben eliminar los que no están activos
+            lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
+}
