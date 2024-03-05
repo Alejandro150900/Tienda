fed2b4a16155c26988371eceb0b6c42f944d5b27
Alejandro150900 <luisaa00-@hotmail.com>
2024-03-05 00:00:25 -0600

    Semana 6 proyecto tienda

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,24 +1,44 @@
 package com.tienda.service.impl;
- 
+
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
+import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.tienda.service.CategoriaService;
- 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
-    //La anotación autowired crea un único objeto mientras se ejecuta la app
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
-    public List<Categoria> getCategorias(boolean activos){
-        var lista=categoriaDao.findAll();
-        if(activos){ //Se deben eliminar los que no están activos
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
             lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
